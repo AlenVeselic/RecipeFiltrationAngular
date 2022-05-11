e478f2ce4192fb4b411f96d15867f67ae7e8d146
Alen <alen.veselic01@gmail.com>
2022-05-11 20:57:56 +0200

    Add grid componenet for displaying grid of recipes

---
+++ src/app/result-grid/result-grid.component.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { FilterService } from '../services/filter.service';
+
+@Component({
+  selector: 'app-result-grid',
+  templateUrl: './result-grid.component.html',
+  styleUrls: ['./result-grid.component.scss']
+})
+export class ResultGridComponent implements OnInit {
+
+  @Input() recipes: string[] = [];
+
+  phpRecipes: any[] = [];
+
+  private recipeSubscription: Subscription;
+
+  constructor(private http: HttpClient, private router: Router, private filterService: FilterService) { 
+    this.recipeSubscription = this.filterService.getRecipeUpdate().subscribe( recipes =>
+      {
+        this.phpRecipes = recipes
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    console.log("Sending request")
+    if(this.phpRecipes.length === 0){
+    this.http.get("http://localhost:8000/api/v1/recipes").subscribe((response: any) => {this.phpRecipes = response})
+    }
+    for(let recipe of this.phpRecipes) console.log(recipe.ime)
+  }
+
+  
+
+
+}
